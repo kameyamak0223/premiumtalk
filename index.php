3516da5f0b79569cb39f573c4e1ed471cb0f8d3d
kameyamak0223 <kameyamak0223@gmail.com>
2017-12-15 16:37:20 +0900

    修正

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 require_once __DIR__ . '/vendor/autoload.php';
 
 $http_client = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('CHANNEL_ACCESS_TOKEN'));
-$bot = new \LINE\LINEBot($http_client, array('channnel_secret' => getenv('CHANNEL_SECRET')));
+$bot = new \LINE\LINEBot($http_client, array('channel_secret' => getenv('CHANNEL_SECRET')));
 
 $signature = $_SERVER['HTTP_' . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
 
